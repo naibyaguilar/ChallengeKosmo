b22646cab5b4c3b9a21bbd6a2a5a0ae9a23fe684
Naiby <naiby.aguilar@stackitgroup.com>
2023-06-21 17:59:47 -0600

    challenge done

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Container from "./components/container/Container";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Container />
+      </header>
+    </div>
+  );
+}
+
+export default App;
